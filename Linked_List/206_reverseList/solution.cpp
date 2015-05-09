1b1970e0ca2899bcd1e5e66deda27a7ddcfc5596
Haihua Xu <xuhaihua90@gmail.com>
2015-05-09 13:33:34 -0700

    update sol to 206_reversedList

--- Linked_List/206_reverseList/solution.cpp
+++ Linked_List/206_reverseList/solution.cpp
@@ -8,17 +8,21 @@
  */
 class Solution {
 public:
-  ListNode* reverseList(ListNode* head) {
-    if(head == NULL || head->next == NULL){
-      return head;
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* pre = new ListNode(0);
+        pre->next = head;
+        
+        ListNode* tmp = NULL;
+        while(head->next != NULL){
+            tmp = head->next;
+            head->next = tmp->next;
+            tmp->next = pre->next;
+            pre->next = tmp;
+        }
+        
+        return pre->next;
     }
-    ListNode* prev = NULL;
-    while(head != 0){
-      ListNode* next = head->next;
-      head->next = prev;
-      prev = head;
-      head = next;
-    }
-    return prev;
-  }
 };
