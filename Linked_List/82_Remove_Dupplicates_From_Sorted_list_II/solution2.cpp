194965e7f77a4291bc3ea615629ed4fa86be3701
Haihua Xu <xuhaihua90@gmail.com>
2015-05-08 21:00:34 -0700

    add comment to sol2

--- Linked_List/82_Remove_Dupplicates_From_Sorted_list_II/solution2.cpp
+++ Linked_List/82_Remove_Dupplicates_From_Sorted_list_II/solution2.cpp
@@ -18,28 +18,30 @@ Given 1->1->1->2->3, return 2->3.
 
 class Solution {
 public:
-  ListNode *deleteDuplicates(ListNode *head) {
-    // create dummy node to normailze the process, ensure node->next never be null.
-    ListNode* dummy = new ListNode(0);
-    dummy->next = head;
-    ListNode* node = dummy;
+    ListNode *deleteDuplicates(ListNode *head) {
+        // create dummy node to normailze the process, ensure node->next never be null.
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* node = dummy;
         
-    // main function, ends when there are less than 2 nodes follows.
-    while(node->next!=NULL && node->next->next!=NULL){
-      if(node->next->val == node->next->next->val){
-	int val = node->next->val;
-	while(node->next!=NULL && node->next->val == val){
-	  ListNode* toDelete = node->next;
-	  node->next = node->next->next;
-	  delete toDelete;
-	}
-      }
-      else{
-	node = node->next;
-      }
+        // main function, always stands on a valid node and check the followeing 2 nodes. ends when there are less 2.
+        while(node->next!=NULL && node->next->next!=NULL){
+            // flag the duplicate, record the value and delete one by one.
+            if(node->next->val == node->next->next->val){
+                int val = node->next->val;
+                // delete next node
+                while(node->next!=NULL && node->next->val == val){
+                    ListNode* toDelete = node->next;
+                    node->next = node->next->next;
+                    delete toDelete;
+                }
+            }
+            else{
+                node = node->next;
+            }
+        }
+        
+        // always return dummy->head when there is dummy
+        return dummy->next;
     }
-        
-    // always return dummy->head when there is dummy
-    return dummy->next;
-  }
 };
