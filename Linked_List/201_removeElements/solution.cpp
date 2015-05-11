a0c78541174a7af57077147587ccd487a96602a2
Haihua Xu <xuhaihua90@gmail.com>
2015-05-11 14:00:33 -0700

    add sol to 201 remove elements

---
+++ Linked_List/201_removeElements/solution.cpp
@@ -0,0 +1,33 @@
+/*
+Remove all elements from a linked list of integers that have value val.
+
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* removeElements(ListNode* head, int val) {
+    ListNode *dummy = new ListNode(0);
+    dummy->next = head;
+    ListNode *node = dummy;
+    while(node->next){
+      if(node->next->val == val){
+	node->next = node->next->next;
+      }
+      else{
+	node = node->next;
+      }
+    }
+    return dummy->next;
+  }
+};
