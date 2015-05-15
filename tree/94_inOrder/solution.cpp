dae8aae91af2dcfa98256581826e7b5208783b4b
Haihua Xu <xuhaihua90@gmail.com>
2015-05-15 16:44:00 -0700

    add sol to 94

---
+++ tree/94_inOrder/solution.cpp
@@ -0,0 +1,42 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> v;
+    inOrder(root, v);
+    return v;
+  }
+    
+  void inOrder(TreeNode* root, vector<int> &v){
+    if(!root) return;
+    inOrder(root->left, v);
+    v.push_back(root->val);
+    inOrder(root->right, v);
+  }
+};
