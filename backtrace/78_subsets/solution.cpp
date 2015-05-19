4fc01f6f9f987e25efb3d40f6ad0843aa151659e
Haihua Xu <xuhaihua90@gmail.com>
2015-05-19 13:58:49 -0700

    edit the 78 subsets

--- backtrace/78_subsets/solution.cpp
+++ backtrace/78_subsets/solution.cpp
@@ -19,25 +19,35 @@ If nums = [1,2,3], a solution is:
 ]
 */
 
+// Thanks http://algorithm.yuanbin.me/backtracking/subsets.html
+// Thanks http://fisherlei.blogspot.com/2013/01/leetcode-subsets.html
+
 class Solution {
 public:
-  vector<vector<int>> subsets(vector<int>& nums) {
-    vector<vector<int> >result;
-    if(nums.empty()){
-      return result;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> >resultSet;
+        if(nums.empty()){
+            return resultSet;
+        }
+        
+        sort(nums.begin(), nums.end());
+        vector<int> aSubset;
+        resultSet.push_back(aSubset);
+        backTrack(nums, 0, aSubset, resultSet);
+        return resultSet;
     }
-    sort(nums.begin(), nums.end());
-    vector<int> list;
-    backTrack(result, list, nums, 0);
-    return result;
-  }
 private:
-  void backTrack(vector<vector<int> >& result, vector<int>& list, vector<int>& nums, int pos){
-    result.push_back(list);
-    for(int i = pos; i < nums.size(); i++){
-      list.push_back(nums[i]);
-      backTrack(result, list, nums, i+1);
-      list.pop_back();
+    void backTrack(vector<int>& oriSet, int pos, vector<int>& aSubset, vector<vector<int> >& resultSet){
+        // for each element in the oriSet starting from pos
+        for(int i = pos; i < oriSet.size(); i++){
+            // add it to the list
+            aSubset.push_back(oriSet[i]);
+            // push this result set.
+            resultSet.push_back(aSubset);
+            // process the remaing element in the oriSet.
+            backTrack(oriSet, i+1, aSubset, resultSet);
+            // assume have itereate this case with oriSet[i], now pop out(exculde) this element and add the next element;
+            aSubset.pop_back();
+        }
     }
-  }
 };
