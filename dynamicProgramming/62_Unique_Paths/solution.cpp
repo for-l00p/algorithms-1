124734f00926d101c73d4ae130300cac091c8191
Haihua Xu <xuhaihua90@gmail.com>
2015-05-28 18:20:44 -0700

    add sol to 62 unique paths

---
+++ dynamicProgramming/62_Unique_Paths/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int uniquePaths(int m, int n) {
+    int arr[n];
+    for(int i = 0; i < m; i++){
+      for(int j = 0; j < n; j++){
+	if(j == 0 || i == 0){
+	  arr[j] = 1;
+	}
+	else{
+	  // self + left
+	  arr[j] = arr[j] + arr[j-1];
+	}
+      }
+    }
+    return arr[n-1];
+  }
+};
