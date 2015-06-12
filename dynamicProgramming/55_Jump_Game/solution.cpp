bbfedbc996c0cb62c9874fbd71168af17ff3eaf2
Haihua Xu <xuhaihua90@gmail.com>
2015-06-11 18:20:22 -0700

    add sol to 55 jump game

---
+++ dynamicProgramming/55_Jump_Game/solution.cpp
@@ -0,0 +1,24 @@
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false.
+*/
+
+class Solution {
+public:
+  bool canJump(vector<int>& nums) {
+    int boundryIndex = 0;
+    for(int i = 0; i <= boundryIndex && i < nums.size(); i++){
+      if(boundryIndex >= nums.size()-1) return true;
+      boundryIndex = max(boundryIndex, i + nums[i]);
+    }
+    return false;
+  }
+};
